6d85f6c21297de9d66f4b027838491ec611b5526
Joao Pedro <jpsdb10@hotmail.com>
2023-06-13 20:54:40 -0300

    create project and implementing Class

---
+++ src/Contador.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner terminal = new Scanner(System.in);
+
+        int parametroUm = terminal.nextInt();
+        int parametroDois = terminal.nextInt();
+
+        try {
+            // chamar metodo de contagem
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // validar se parametroUm eh maior que parametroDois
+
+        //realizar a impressao dos elementos
+    }
+}
